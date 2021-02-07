1c1d4edb3b46f1b121e863ad87349aceb65fb97b
Zimm <antoxa.zimm@gmail.com>
2021-02-07 20:11:33 +0100

    ch 09

---
+++ src/MarkJPriceCSharp9DotNet5.Ch09/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarkJPriceCSharp9DotNet5.Ch09
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
